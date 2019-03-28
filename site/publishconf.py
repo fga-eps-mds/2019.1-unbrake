40d69176b152b8992a11ce2d805d52fdbde579c3
bumbleblo <bumbleblo2013@gmail.com>
2019-03-28 03:03:21 -0300

    Adding new siteurl config

--- site/publishconf.py
+++ site/publishconf.py
@@ -11,7 +11,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://fga-eps-mds.github.io/2019.1-unbrake/'
+#SITEURL = 'https://fga-eps-mds.github.io/2019.1-unbrake/'
+SITEURL = '.'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
