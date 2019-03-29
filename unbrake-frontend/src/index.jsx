d9dd36aeccaf49802dddcff491ee749891edfa76
Ícaro Pires <icaropsa@gmail.com>
2019-03-29 17:41:31 -0300

    Add initial rules, beyond the defaults for lintint
    Related issue: #30
    
    Co-authored-by: João Robson <joaorobsonmartins@gmail.com>

--- unbrake-frontend/src/index.jsx
+++ unbrake-frontend/src/index.jsx
@@ -6,7 +6,9 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+/*
+ * If you want your app to work offline and load faster, you can change
+ * unregister() to register() below. Note this comes with some pitfalls.
+ * Learn more about service workers: https://bit.ly/CRA-PWA
+ */
 serviceWorker.unregister();
--- unbrake-frontend/src/serviceWorker.js
+++ unbrake-frontend/src/serviceWorker.js
@@ -1,14 +1,20 @@
-// This optional code is used to register a service worker.
-// register() is not called by default.
+/*
+ * This optional code is used to register a service worker.
+ * register() is not called by default.
+ */
 
-// This lets the app load faster on subsequent visits in production, and gives
-// it offline capabilities. However, it also means that developers (and users)
-// will only see deployed updates on subsequent visits to a page, after all the
-// existing tabs open on the page have been closed, since previously cached
-// resources are updated in the background.
+/*
+ * This lets the app load faster on subsequent visits in production, and gives
+ * it offline capabilities. However, it also means that developers (and users)
+ * will only see deployed updates on subsequent visits to a page, after all the
+ * existing tabs open on the page have been closed, since previously cached
+ * resources are updated in the background.
+ */
 
-// To learn more about the benefits of this model and instructions on how to
-// opt-in, read https://bit.ly/CRA-PWA
+/*
+ * To learn more about the benefits of this model and instructions on how to
+ * opt-in, read https://bit.ly/CRA-PWA
+ */
 
 const isLocalhost = Boolean(
   window.location.hostname === "localhost" ||
@@ -27,37 +33,43 @@ function registerValidSW(swUrl, config) {
       // eslint-disable-next-line no-param-reassign
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
-        if (installingWorker == null) {
+        if (installingWorker === null) {
           return;
         }
         installingWorker.onstatechange = () => {
           if (installingWorker.state === "installed") {
             if (navigator.serviceWorker.controller) {
-              // At this point, the updated precached content has been fetched,
-              // but the previous service worker will still serve the older
-              // content until all client tabs are closed.
-              // console.log(
-              //   "New content is available and will be used when all " +
-              //     "tabs for this page are closed. See https://bit.ly/CRA-PWA."
-              // );
+              /*
+               * At this point, the updated precached content has been fetched,
+               * but the previous service worker will still serve the older
+               * content until all client tabs are closed.
+               * console.log(
+               *   "New content is available and will be used when all " +
+               *     "tabs for this page are closed. See https://bit.ly/CRA-PWA."
+               * );
+               */
 
               // Execute callback
               if (config && config.onUpdate) {
                 config.onUpdate(registration);
               }
             } else if (config && config.onSuccess) {
-              // At this point, everything has been precached.
-              // It"s the perfect time to display a
-              // "Content is cached for offline use." message.
-              // console.log("Content is cached for offline use.");
+              /*
+               * At this point, everything has been precached.
+               * It"s the perfect time to display a
+               * "Content is cached for offline use." message.
+               * console.log("Content is cached for offline use.");
+               */
 
               // Execute callback
               config.onSuccess(registration);
             } else {
-              // At this point, everything has been precached.
-              // It"s the perfect time to display a
-              // "Content is cached for offline use." message.
-              // console.log("Content is cached for offline use.");
+              /*
+               * At this point, everything has been precached.
+               * It"s the perfect time to display a
+               * "Content is cached for offline use." message.
+               * console.log("Content is cached for offline use.");
+               */
             }
           }
         };
@@ -74,9 +86,12 @@ function checkValidServiceWorker(swUrl, config) {
     .then(response => {
       // Ensure service worker exists, and that we really are getting a JS file.
       const contentType = response.headers.get("content-type");
+      const notFoundHttpStatus = 404;
+      const notFoundInArray = -1;
       if (
-        response.status === 404 ||
-        (contentType != null && contentType.indexOf("javascript") === -1)
+        response.status === notFoundHttpStatus ||
+        (contentType !== null &&
+          contentType.indexOf("javascript") === notFoundInArray)
       ) {
         // No service worker found. Probably a different app. Reload the page.
         navigator.serviceWorker.ready.then(registration => {
@@ -90,9 +105,11 @@ function checkValidServiceWorker(swUrl, config) {
       }
     })
     .catch(() => {
-      // console.log(
-      //   "No internet connection found. App is running in offline mode."
-      // );
+      /*
+       * console.log(
+       *   "No internet connection found. App is running in offline mode."
+       * );
+       */
     });
 }
 
@@ -101,9 +118,11 @@ export function register(config) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won"t work if PUBLIC_URL is on a different origin
-      // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebook/create-react-app/issues/2374
+      /*
+       * Our service worker won"t work if PUBLIC_URL is on a different origin
+       * from what our page is served on. This might happen if a CDN is used to
+       * serve assets; see https://github.com/facebook/create-react-app/issues/2374
+       */
       return;
     }
 
@@ -114,13 +133,17 @@ export function register(config) {
         // This is running on localhost. Let"s check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
 
-        // Add some additional logging to localhost, pointing developers to the
-        // service worker/PWA documentation.
+        /*
+         * Add some additional logging to localhost, pointing developers to the
+         * service worker/PWA documentation.
+         */
         navigator.serviceWorker.ready.then(() => {
-          // console.log(
-          //   "This web app is being served cache-first by a service " +
-          //     "worker. To learn more, visit https://bit.ly/CRA-PWA"
-          // );
+          /*
+           * console.log(
+           *   "This web app is being served cache-first by a service " +
+           *     "worker. To learn more, visit https://bit.ly/CRA-PWA"
+           * );
+           */
         });
       } else {
         // Is not localhost. Just register service worker
