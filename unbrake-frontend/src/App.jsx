5e898b32f4bf0c663445da03d9aea2b771e4b040
Tiago Miguel <tmcstiago@gmail.com>
2019-04-06 18:19:10 -0300

    Changing structure upload file

--- unbrake-frontend/src/App.jsx
+++ unbrake-frontend/src/App.jsx
@@ -4,18 +4,18 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./reducer/index";
 // import RealTimeChart from "./components/RealTimeChart";
+import UploadFile from "./components/Configuration";
 
 import SideBarMenu from "./components/SideBarMenu";
 import Routes from "./Routes";
+import Configuration from "./components/Configuration";
 
 const store = createStore(reducers);
 
 const App = () => (
   <Provider store={store}>
-    <React.Fragment>
-      <SideBarMenu />
-      <Routes />
-    </React.Fragment>
+    {console.log(store)}
+    <Configuration />
   </Provider>
 );
 
--- unbrake-frontend/src/components/Configuration.jsx
+++ unbrake-frontend/src/components/Configuration.jsx
@@ -1,34 +1,70 @@
 import React from "react";
-import { FileUpload } from "redux-file-upload";
-import { reduxForm } from "redux-form";
+import axios, { post } from "axios";
+import iniparser from "iniparser";
 
-const styles = theme => ({
-  root: {
-    flexGrow: 1
+class SimpleReactFileUpload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null
+    };
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.fileUpload = this.fileUpload.bind(this);
   }
-});
 
-const UploadFile = props => {
-  const { handleSubmit } = props;
+  onFormSubmit(e) {
+    e.preventDefault(); // Stop form submit
+    this.fileUpload(this.state.file).then(response => {
+      console.log(response.data);
+    });
+  }
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <form onSubmit={handleSubmit}>
-          <FileUpload
-            allowedFileTypes={["doc", "pdf"]}
-            data={{ type: "picturefile" }}
-            dropzoneId="fileUpload"
-          >
-            <button>Upload</button>
-          </FileUpload>
-        </form>
-      </header>
-    </div>
-  );
-  // }
-};
+  onChange(e) {
+    // this.setState({file:e.target.files[0]})
+    this.fileUpload(e);
+  }
 
-export default reduxForm({
-  form: "login"
-})(UploadFile);
+  fileUpload(file) {
+    const url = "http://localhost:3000/";
+    const formData = new FormData();
+    // alert(formData.result);
+    const fr = new FileReader();
+    formData.append("file", file);
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      const content = e.target.result;
+      const configa = iniparser.parseString(content);
+      console.log(configa);
+      // Here the content has been read successfuly
+
+      alert(content);
+    };
+    reader.onerror = function(e) {
+      alert(e);
+    };
+    reader.readAsText(file, "UTF-8");
+
+    const config = {
+      headers: {
+        "content-type": "multipart/form-data"
+      }
+    };
+    console.log("F", fr, fr.result);
+    this.setState({ f: fr });
+    console.log(this.state);
+    // return  post(url, formData,config)
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <h1>File Upload</h1>
+        <input type="file" onChange={e => this.onChange(e.target.files[0])} />
+        <button type="submit">Upload</button>
+      </form>
+    );
+  }
+}
+
+export default SimpleReactFileUpload;
