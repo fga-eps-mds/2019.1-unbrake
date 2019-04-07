478596a4aa4c8d967081e6943747faa21a0f9405
leticiaarj <leticiards.04@gmail.com>
2019-04-07 19:44:25 -0300

    Functions for selecting an attribute

--- unbrake-api/configuration/schema.py
+++ unbrake-api/configuration/schema.py
@@ -27,9 +27,17 @@ class AuxiliaryOutputType(DjangoObjectType):
 class Query(object):
     cycles = graphene.Field(CyclesType, id=graphene.Int(), CyclesNumber=graphene.Int(), CyclesTime=graphene.Int() )
     all_cycles = graphene.List(CyclesType, id=graphene.Int())
+
+    velocity = graphene.Field(VelocityType, id=graphene.Int(), UpperLimit=graphene.Int(), InferiorLimit=graphene.Int() )
     all_velocity = graphene.List(VelocityType)
+
+    wait = graphene.Field(WaitType, id=graphene.Int(), UpperTime=graphene.Int(), InferiorTime=graphene.Int() )
     all_wait = graphene.List(WaitType)
+
+    shutdown = graphene.Field(ShutdownType, id=graphene.Int(), DisablesShutdown=graphene.Boolean() )
     all_shutdown = graphene.List(ShutdownType)
+
+    auxiliaryoutput = graphene.Field(AuxiliaryOutputType, id=graphene.Int(), EnableOutput=graphene.Boolean(), Temperature=graphene.Float(), Time=graphene.Float() )
     all_auxiliaryoutuput = graphene.List(AuxiliaryOutputType)
 
     def resolve_all_cycles(self, info, ** kwargs):
@@ -40,17 +48,48 @@ class Query(object):
 
         if id is not None:
             return Cycles.objects.get(pk=id)
-
         return None
+
 
     def resolve_all_velocity(self, info, ** kwargs):
         return Velocity.objects.all()
 
+    def resolve_velocity(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Velocity.objects.get(pk=id)
+        return None
+
+
     def resolve_all_wait(self, info, ** kwargs):
         return  Wait.objects.all()
 
+    def resolve_wait(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Wait.objects.get(pk=id)
+        return None
+
+
     def resolve_all_shutdown(self, info, ** kwargs):
         return  Shutdown.objects.all()
+
+    def resolve_shutdown(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Shutdown.objects.get(pk=id)
+        return None
         
+
     def resolve_all_auxiliaryoutput(self, info, ** kwargs):
         return  AuxiliaryOutput.objects.all()
+
+    def resolve_auxiliaryoutput(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return AuxiliaryOutput.objects.get(pk=id)
+        return None
