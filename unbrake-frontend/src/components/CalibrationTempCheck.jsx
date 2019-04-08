17aed3ce5f4ba48abfc3475df9ba449148d787d1
Victor Levi Peixoto <victorlevipeixoto@gmail.com>
2019-04-08 10:59:39 -0300

    Bug fix

--- unbrake-frontend/src/components/CalibrationTempCheck.jsx
+++ unbrake-frontend/src/components/CalibrationTempCheck.jsx
@@ -2,14 +2,10 @@ import React from "react";
 import "../App.css";
 import { Checkbox, FormControlLabel } from "@material-ui/core";
 import { connect } from "react-redux";
-import PropTypes from "prop-types";
+import PropTypes, { func } from "prop-types";
 import { changeGraphic1, changeGraphic2 } from "../actions/ConfigActions";
 
 const FIRST_CHECK_ID = "FirstCheck";
-const mapDispatchToProps = () => ({
-  changeGraphic1,
-  changeGraphic2
-});
 
 class CalibrationTempCheck extends React.Component {
   constructor(props) {
@@ -24,7 +20,13 @@ class CalibrationTempCheck extends React.Component {
   }
 
   renderCheck() {
-    const { checkID, graphic1, graphic2 } = this.props;
+    const {
+      checkID,
+      graphic1,
+      graphic2,
+      enableGraphic1,
+      enableGraphic2
+    } = this.props;
     if (checkID === FIRST_CHECK_ID)
       return (
         <div>
@@ -32,7 +34,7 @@ class CalibrationTempCheck extends React.Component {
             control={
               <Checkbox
                 checked={graphic1}
-                onChange={(event, value) => changeGraphic1(value)}
+                onChange={(event, value) => enableGraphic1(value)}
                 indeterminate
               />
             }
@@ -47,7 +49,7 @@ class CalibrationTempCheck extends React.Component {
           control={
             <Checkbox
               checked={graphic2}
-              onChange={(event, value) => changeGraphic2(value)}
+              onChange={(event, value) => enableGraphic2(value)}
               indeterminate
             />
           }
@@ -66,17 +68,26 @@ class CalibrationTempCheck extends React.Component {
 CalibrationTempCheck.propTypes = {
   checkID: PropTypes.string,
   graphic1: PropTypes.bool,
-  graphic2: PropTypes.bool
+  graphic2: PropTypes.bool,
+  enableGraphic1: func,
+  enableGraphic2: func
 };
 CalibrationTempCheck.defaultProps = {
   checkID: "",
   graphic1: false,
-  graphic2: false
+  graphic2: false,
+  enableGraphic1: () => {},
+  enableGraphic2: () => {}
 };
 
 const mapStateToProps = state => ({
   graphic1: state.configReducer.graphic1,
   graphic2: state.configReducer.graphic2
+});
+
+const mapDispatchToProps = dispatch => ({
+  enableGraphic1: value => dispatch(changeGraphic1(value)),
+  enableGraphic2: value => dispatch(changeGraphic2(value))
 });
 
 export default connect(
