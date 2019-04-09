2149edac17fcf6f92973fe2715fd83ae2d066f47
icaropires <icaropsa@gmail.com>
2019-04-09 16:20:03 -0300

    Small fixes for checking tools

--- unbrake-api/unbrake_api/schema.py
+++ unbrake-api/unbrake_api/schema.py
@@ -1,10 +1,16 @@
-import graphene
+'''
+Needed for GraphQL configuration
+'''
 
+import graphene
 import configuration.schema
 
-class Query(configuration.schema.Query, graphene.ObjectType):
-    # This class will inherit from multiple Queries
-    # as we begin to add more apps to our project
-   pass
 
-schema = graphene.Schema(query=Query)
+class Query(configuration.schema.Query, graphene.ObjectType):
+    '''
+    This class will inherit from multiple Queries
+    as we begin to add more apps to our project
+    '''
+
+
+schema = graphene.Schema(query=Query)  # pylint: disable=invalid-name
--- unbrake-api/unbrake_api/urls.py
+++ unbrake-api/unbrake_api/urls.py
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url, include
+from django.conf.urls import url
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
-    #url(r'^admin/', admin.site.urls),
     url(r'^graphql', GraphQLView.as_view(graphiql=True)),
 ]
