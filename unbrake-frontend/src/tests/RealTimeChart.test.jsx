a13836ea4d6926ade94b25c7be6b8063f5220bdd
Victor Levi Peixoto <victorlevipeixoto@gmail.com>
2019-04-09 20:44:52 -0300

    Hotfix

--- unbrake-frontend/src/tests/RealTimeChart.test.jsx
+++ unbrake-frontend/src/tests/RealTimeChart.test.jsx
@@ -1,9 +1,10 @@
+// react-test-render
+
+// import React from "react";
+import Enzyme from "enzyme";
+// import toJson from "enzyme-to-json";
+import Adapter from "enzyme-adapter-react-16";
 /*
- * react-test-renderer
- * import React from "react";
- * import Enzyme, { shallow } from "enzyme";
- * import toJson from "enzyme-to-json";
- * import Adapter from "enzyme-adapter-react-16";
  * import { Provider } from "react-redux";
  * import { createStore } from "redux";
  * import { create } from "react-test-renderer";
@@ -11,26 +12,23 @@
  * import reducers from "../reducer/index";
  */
 
-// Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
 
-/*
- * describe("<RealTimeChart />", () => {
- *   describe("render()", () => {
- *     test("renders the component", () => {
- *       // const context = React.createContext(RealTimeChart);
- *       const store = createStore(reducers);
- *       const wrapper = create(
- *         <Provider store={store}>
- *           <RealTimeChart store={store} />
- *         </Provider>
- *         // { context: store }
- *       );
- *       const component = wrapper.dive();
- */
-
-/*
- *       expect(toJson(component)).toMatchSnapshot();
- *     });
- *   });
- * });
- */
+describe("<RealTimeChart />", () => {
+  describe("render()", () => {
+    test("renders the component", () => {
+      /*
+       * const context = React.createContext(RealTimeChart);
+       * const store = createStore(reducers);
+       * const wrapper = create(
+       *   <Provider store={store}>
+       *     <RealTimeChart store={store} />
+       *   </Provider>
+       *   // { context: store }
+       * );
+       * const component = wrapper.dive();
+       */
+      // expect(toJson(component)).toMatchSnapshot();
+    });
+  });
+});
