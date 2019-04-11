3735f603a00d8c8a5775da5355f97e985ccef464
Victor Levi Peixoto <victorlevipeixoto@gmail.com>
2019-04-10 23:00:18 -0300

    Hotfix

--- unbrake-frontend/src/tests/RealTimeChart.test.jsx
+++ unbrake-frontend/src/tests/RealTimeChart.test.jsx
@@ -1,34 +1,32 @@
 // react-test-render
 
-// import React from "react";
+import React from "react";
 import Enzyme from "enzyme";
-// import toJson from "enzyme-to-json";
+import toJson from "enzyme-to-json";
 import Adapter from "enzyme-adapter-react-16";
-/*
- * import { Provider } from "react-redux";
- * import { createStore } from "redux";
- * import { create } from "react-test-renderer";
- * import RealTimeChart from "../components/RealTimeChart";
- * import reducers from "../reducer/index";
- */
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { create } from "react-test-renderer";
+import RealTimeChart from "../components/RealTimeChart";
+import reducers from "../reducer/index";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("<RealTimeChart />", () => {
   describe("render()", () => {
     test("renders the component", () => {
-      /*
-       * const context = React.createContext(RealTimeChart);
-       * const store = createStore(reducers);
-       * const wrapper = create(
-       *   <Provider store={store}>
-       *     <RealTimeChart store={store} />
-       *   </Provider>
-       *   // { context: store }
-       * );
-       * const component = wrapper.dive();
-       */
-      // expect(toJson(component)).toMatchSnapshot();
+      const context = React.createContext();
+      const store = createStore(reducers);
+      const wrapper = create(
+        <Provider context={context} store={store}>
+          <RealTimeChart context={context} />
+        </Provider>
+        // { context: store }
+      );
+      // const component = wrapper.dive();
+
+      expect(toJson(wrapper)).toMatchSnapshot();
     });
   });
 });
