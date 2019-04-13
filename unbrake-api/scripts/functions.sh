47ce4b6dd0ffc54de50322cb14853f5ab3d53758
Ícaro Pires <icaropsa@gmail.com>
2019-04-12 21:52:34 -0300

    Modularize checking scripts for api

---
+++ unbrake-api/scripts/functions.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+source scripts/scripts_environments
+
+run_command(){
+	echo -n "Running $1..."
+	$* &> $tmpOutputFile
+
+	if [ $? = 0 ]; then
+		echo -e " \033[0;32mOK!\033[0m"
+	else
+		echo -e " \033[0;31mFAILED!\033[0m\n"
+		cat $tmpOutputFile
+
+		echo -e "\033[0;31mSome check failed :(\033[0m"
+		echo -e "\033[0;33mHave you tried to rebuilding the image? Try look the tool documentation as well!\033[0m"
+
+        rm -rf $tmpOutputFile
+		exit 1
+	fi
+
+    rm -rf $tmpOutputFile
+}
