10801702c5aa918805b04bb13a4eb5804ff517a4
Ícaro Pires <icaropsa@gmail.com>
2019-04-14 17:57:21 -0300

    Add script for collecting coverage on API

---
+++ unbrake-api/unbrake_api/management/commands/check_tests_coverage.py
@@ -0,0 +1,16 @@
+'''
+Link check_tests script to a manage.py commanD
+'''
+
+import os
+from django.core.management import BaseCommand
+
+
+class Command(BaseCommand):
+    '''
+    Check if tests are OK
+    '''
+    help = "Check if tests are OK"
+
+    def handle(self, *args, **options):
+        os.system('./scripts/check_tests_coverage')
