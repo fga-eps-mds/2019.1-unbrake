1560815e4f03700dd27b17c964083bf5e29272f2
medeiroslucas <medeirosrosalucas@gmail.com>
2019-04-23 14:26:14 -0300

    Remove request.jsx

--- unbrake-frontend/src/utils/request.jsx
+++
@@ -1,9 +0,0 @@
-export default async function request(url, method) {
-  const responser = await fetch(url, {
-    method
-  }).then(response => {
-    return response.json();
-  });
-
-  return responser;
-}
