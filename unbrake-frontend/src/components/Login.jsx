55b4653cc99248cca055e84b481d3812357948ef
joao robson <joaorobsonmartins@gmail.com>
2019-04-30 11:11:43 -0300

    Fix redirect to login page

--- unbrake-frontend/src/components/Login.jsx
+++ unbrake-frontend/src/components/Login.jsx
@@ -4,6 +4,7 @@ import { TextField } from "redux-form-material-ui";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 import { withStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 import Cookies from "universal-cookie";
