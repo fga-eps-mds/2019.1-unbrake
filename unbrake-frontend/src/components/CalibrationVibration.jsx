208307e6552809212055cf9188122a8441e83a05
Tiago Miguel <tmcstiago@gmail.com>
2019-05-21 00:02:46 -0300

    Start issue
    co-authored-by: Lucas Medeiros <medeirosrosalucas@gmail.com>

---
+++ unbrake-frontend/src/components/CalibrationVibration.jsx
@@ -0,0 +1,26 @@
+// import React from "react";
+
+/*
+ * class Vibration extends React.Component {
+ *   constructor(props) {
+ *     super(props);
+ *     // this.state = {
+ *     // };
+ *   }
+ */
+
+//   render() {
+//     return (
+//       <form
+//       /*
+//        * className={classes.container}
+//        * onSubmit={handleSubmit(values => {
+//        *   submit(values, this.state);
+//        * })}
+//        */
+//       />
+//     );
+//   }
+// }
+
+// export default Vibration;
