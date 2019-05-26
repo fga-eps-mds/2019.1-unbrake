f485700c685501700a08502984b734fb77b9e7bd
Tiago Miguel <tmcstiago@gmail.com>
2019-05-26 00:22:51 -0300

    Update Configuration tests
    co-authored-by: Lucas Medeiros <medeirosrosalucas@gmail.com>

--- unbrake-frontend/src/tests/Configuration.test.jsx
+++ unbrake-frontend/src/tests/Configuration.test.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-// import fetch from "jest-fetch-mock";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import Configuration from "../Configuration/Configuration";
 import ComponentTest from "./ComponentTest";
-// import { API_URL_GRAPHQL } from "../utils/Constants";
+import reducers from "../reducer/index";
+
+/*
+ * import fetch from "jest-fetch-mock";
+ * import { API_URL_GRAPHQL } from "../utils/Constants";
+ */
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("<Configuration />", () => {
-  const wrapper = shallow(<Configuration />);
+  const context = React.createContext();
+  const store = createStore(reducers);
+  const wrapper = shallow(
+    <Provider context={context} store={store}>
+      <Configuration context={context} />
+    </Provider>
+  );
+
   ComponentTest(wrapper);
 
   /*
