2b86fb7648792be36f68818b4505a72aa77759ec
Ícaro Pires <icaropsa@gmail.com>
2019-06-15 00:15:52 -0300

    Add mqtt writing key as secret for dev and production/staging

---
+++ unbrake-api/utils/general.py
@@ -0,0 +1,26 @@
+'''
+For keeping general util functions to the whole project
+'''
+
+import os
+
+
+def get_secret(secret_name):
+    '''
+    For getting project secrets which are mount on docker-compose files
+    '''
+
+    key_file = None
+    try:
+        key_file = open(os.path.join('/', 'run', 'secrets', secret_name))
+    except FileNotFoundError:
+        raise FileNotFoundError(
+            f"""
+            ===================================================================
+            '{secret_name}' secret not found.
+            Ask the development team the secrets or use your owns.
+            ===================================================================
+            """
+        )
+
+    return key_file
