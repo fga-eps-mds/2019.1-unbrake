63f7afb8f52a91bb1b16812f7f04c0129c8f414e
Vinicius Ferreira <vfbdelima@gmail.com>
2019-06-22 12:46:40 -0300

    Implement equations for speed and command form
    Signed-off-by: Vinicius Ferreira <vfbdelima@gmail.com>

--- unbrake-frontend/src/tests/Calibration/Speed.test.jsx
+++ unbrake-frontend/src/tests/Calibration/Speed.test.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Speed from "../../calibration/Speed";
 import ComponentTest from "../ComponentTest";
+import reducers from "../../reducer/index";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("<Speed />", () => {
-  const wrapper = shallow(<Speed />);
+  const context = React.createContext();
+  const store = createStore(reducers);
+  const wrapper = shallow(
+    <Provider context={context} store={store}>
+      <Speed />
+    </Provider>
+  );
   ComponentTest(wrapper);
 });
