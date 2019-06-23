e602610abb6b56e933dcb00e9cf5faa0848db6b4
Tiago Miguel <tmcstiago@gmail.com>
2019-06-23 01:56:37 -0300

    Fix errors api

--- unbrake-frontend/src/testModule/TabMenuComponent.jsx
+++ unbrake-frontend/src/testModule/TabMenuComponent.jsx
@@ -3,7 +3,6 @@ import { withStyles } from "@material-ui/core/styles";
 import { AppBar, Grid } from "@material-ui/core";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-// import Typography from "@material-ui/core/Typography";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import RealTimeChart from "../components/RealTimeChart";
